0844e0b2eeac131f6a7a1ead0a04ef30c69e3d6a
Rahul <rahul.kasoju@gmail.com>
2024-10-21 14:38:29 +0530

    first

---
+++ src/main/java/com/demo/book_service/model/Book.java
@@ -0,0 +1,27 @@
+package com.demo.book_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int bookId;
+	private String bookTitle;
+	private String bookPrice;
+	private LocalDate bookPublishedDate;
+	private String bookUrl;
+	private String bookAuthor;
+}
